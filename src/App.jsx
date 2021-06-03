99d558247158733658b48abcd1967b8ff989a5c0
Andrew Moore <mooreandrew015@gmail.com>
2021-06-02 21:11:58 -0400

    needs work

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,42 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 export function App() {
-  return <div>Hello, World!</div>
+  const [currencyResults, setCurrencyResults] = useState({ rates: [] })
+  const [amount, setAmount] = useState(1)
+
+  // @ts-ignore
+  useEffect(async () => {
+    const resp = await axios.get('https://api.exchangeratesapi.io/v1/')
+    ? access_key= 'd85c4f40ebc6f5bddea856130086f488'
+    
+
+    setCurrencyResults(resp.data)
+  }, [])
+
+  return (
+    <div>
+      <ul>
+        {Object.entries(
+          // @ts-ignore
+          currencyResults.rates
+        ).map(([currencyCode, currencyDetails]) => {
+          return (
+            <li key={currencyCode}>
+              {currencyCode} : {(currencyDetails * amount).toFixed(2)}
+            </li>
+          )
+        })}
+      </ul>
+      <input
+        type="number"
+        // @ts-ignore
+        onChange={(event) => setAmount(event.target.value)}
+        value={amount}
+      />
+      <footer>
+        <h1>US $ Currency Converter</h1>
+      </footer>
+    </div>
+  )
 }
